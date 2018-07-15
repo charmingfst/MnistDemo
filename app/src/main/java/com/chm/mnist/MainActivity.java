82b6c4398772e5af2abc38ec419487474df5a1d9
charmingfst <572663328@qq.com>
2018-07-15 11:41:18 +0800

    MnistDemo

---
+++ app/src/main/java/com/chm/mnist/MainActivity.java
@@ -0,0 +1,161 @@
+package com.chm.mnist;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * Created by tsiang on 2016/11/26.
+ */
+
+public class MainActivity  extends Activity implements View.OnClickListener{
+    private static final String TAG = "MainActivity";
+
+    private Button imgChooseButton,startRecognitionButton;
+    private ImageView imageView;
+    private TextView statusTextView;
+    private Bitmap bitmap;
+
+    RecognitionUtil preTF;
+    private static final String MODEL_FILE = "file:///android_asset/mnist.pb"; //模型存放路径
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        requestReadExternalPermission();
+
+        setContentView(R.layout.activity_main);
+
+
+        imageView = (ImageView) findViewById(R.id.imgView);
+        imgChooseButton = (Button) findViewById(R.id.imgChooseButton);
+        startRecognitionButton = (Button) findViewById(R.id.startRecognitionButton);
+        startRecognitionButton.setVisibility(View.GONE);
+        statusTextView = (TextView) findViewById(R.id.statusTextView);
+        imgChooseButton.setOnClickListener(this);
+        startRecognitionButton.setOnClickListener(this);
+
+        preTF =new RecognitionUtil(getAssets(),MODEL_FILE);//输入模型存放路径，并加载TensoFlow模型
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == imgChooseButton){
+            chooseFile();
+            imageView.setVisibility(View.INVISIBLE);
+        }else if(v == startRecognitionButton){
+            recognition();
+        }
+    }
+
+    private void chooseFile() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult( Intent.createChooser(intent, "选择文件"), 1);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "请先安装一个文件管理器.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 1) {
+                Uri uri = data.getData();
+                Log.i(TAG,"uri:"+uri);
+
+                try {
+                    String realPath = FileUtils.getPath(this,uri);
+                    Log.i(TAG,"realPath:"+realPath);
+                    statusTextView.setVisibility(View.VISIBLE);
+                    if(realPath == null) {
+                        statusTextView.setText("文件不存在，请重新选择！");
+                        imgChooseButton.setText("重新选择文件");
+                    }else if(!(realPath.endsWith(".jpg") || realPath.endsWith(".png") || realPath.endsWith(".bmp"))) {
+                        statusTextView.setText("文件必须是jpg/png/bmp格式的，请重新选择！");
+                        imgChooseButton.setText("重新选择文件");
+                    }else{
+                        bitmap = BitmapFactory.decodeFile(realPath,null);
+                        imageView.setVisibility(View.VISIBLE);
+                        imageView.setImageBitmap(bitmap);
+                        statusTextView.setText("当前选择文件:"+realPath);
+                        startRecognitionButton.setVisibility(View.VISIBLE);
+                    }
+
+                }catch (SecurityException e){
+                    statusTextView.setText("应用需要读取SD卡，请先授权读取SD卡！");
+                }catch (Exception e){
+                    Log.w(TAG,"onActivityResult:"+e);
+                    statusTextView.setText("选择文件出错:"+e.getMessage());
+                }
+            }
+        }
+    }
+
+
+    private void recognition() {
+        String res="预测结果为：";
+        int[] result= preTF.getPredict(bitmap);
+        for (int i=0;i<result.length;i++){
+            res=res+String.valueOf(result[i])+" ";
+        }
+        String outSoftmax = res;
+        statusTextView.setText(res);
+        Toast.makeText(this,"outSoftmax:"+ outSoftmax,Toast.LENGTH_LONG).show();
+    }
+
+    /*private void recognition() {
+        try {
+            RecognitionService recognitionService = new RecognitionService(MainActivity.this);
+
+            float[] outSoftmax = recognitionService.recognize(bitmap);
+
+            statusTextView.setText("识别结果："+recognitionService.argmax(outSoftmax));
+
+            Toast.makeText(this,"outSoftmax:"+ Arrays.toString(outSoftmax),Toast.LENGTH_LONG).show();
+
+            startRecognitionButton.setVisibility(View.GONE);
+            imgChooseButton.setText("继续选择文件");
+            imgChooseButton.setVisibility(View.VISIBLE);
+        } catch (Exception e) {
+            Log.e(TAG, "识别中发生异常:" + e);
+
+            statusTextView.setText("识别中发生异常！"+e.getMessage());
+
+            startRecognitionButton.setVisibility(View.GONE);
+            imgChooseButton.setText("重新选择文件");
+            imgChooseButton.setVisibility(View.VISIBLE);
+        }
+    }*/
+
+
+
+    /**
+     * 读取SD卡需要用户授权。
+     */
+    @SuppressLint("NewApi")
+    private void requestReadExternalPermission() {
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "READ permission IS NOT granted...");
+
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+
+        } else {
+            Log.d(TAG, "READ permission is granted...");
+        }
+    }
+}
